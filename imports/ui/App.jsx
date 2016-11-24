1a450c05fc341be74d33282ddf6eb2fec8439a88
River Kanies <river.kanies@gmail.com>
2016-11-24 09:45:50 -0500

    editable matrix

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,6 +6,17 @@ import { Tasks } from '../api/tasks.js'
 import Task from './Task.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+    this.state.mtx = [
+      [null, 'price', 'usability'],
+      ['slack', 5, 8],
+      ['IRC', 10, 5]
+    ]
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -13,23 +24,30 @@ class App extends Component {
   }
 
   renderLabelRow() {
-    const labelRow = [' '].concat(this.variables())
-    labelRow.push('score')
-    return <tr>{this.renderRow(labelRow)}</tr>
+    const labelRow = this.state.mtx[0].concat(['score'])
+    return <tr>{this.renderRow(labelRow,0)}</tr>
   }
 
-  renderRows() {
-    return this.options().map((opt, i) => {
-      const score = opt.vals.reduce((a,b) => (a+b), 0)
-      const row = [opt.name].concat(opt.vals).concat(score)
-      return <tr key={i}>{this.renderRow(row)}</tr>
+  renderOptionRows() {
+    //console.log('state', this.state)
+    return this.state.mtx.slice(1).map((row, i) => {
+      //console.log('row', row, i)
+      const score = row.slice(1).reduce((a,b) => (parseInt(a)+parseInt(b) ), 0)
+      const rowScored = row.concat(score)
+      return <tr key={i}>{this.renderRow(rowScored,i+1)}</tr>
     })
   }
 
-  renderRow(row) {
-    return row.map((cell, i) => (
-      <td key={i}>{cell}</td>
+  renderRow(row,i) {
+    return row.map((cell, j) => (
+      <td key={j}>{this.renderCell(cell,i,j)}</td>
     ))
+  }
+
+  renderCell(val,i,j) {
+    if (val === null) return
+    if (j === this.numColumns()) return val
+    return <input value={val} onChange={this.onChangeHandler(i,j)}/>
   }
 
   render() {
@@ -42,7 +60,7 @@ class App extends Component {
         <table>
           <tbody>
             {this.renderLabelRow()}
-            {this.renderRows()}
+            {this.renderOptionRows()}
           </tbody>
         </table>
 
@@ -57,15 +75,16 @@ class App extends Component {
     )
   }
 
-  variables() {
-    return ['price', 'usability']
+  onChangeHandler(i,j) {
+    return (e) => {
+      const { mtx } = this.state
+      mtx[i][j] = e.target.value
+      this.setState(mtx: mtx)
+    }
   }
 
-  options() {
-    return [
-      {name: 'slack', vals: [5, 8]},
-      {name: 'IRC', vals: [10, 5]}
-    ]
+  numColumns() {
+    return this.state.mtx[0].length
   }
 }
 
