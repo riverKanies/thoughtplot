a8e8c24b3b9edef7619a1ed46e64defd2c9a7a2e
River Kanies <river.kanies@gmail.com>
2016-11-24 07:47:17 -0500

    simple matrix component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -12,9 +12,40 @@ class App extends Component {
     ))
   }
 
+  renderLabelRow() {
+    const labelRow = [' '].concat(this.variables())
+    labelRow.push('score')
+    return <tr>{this.renderRow(labelRow)}</tr>
+  }
+
+  renderRows() {
+    return this.options().map((opt, i) => {
+      const score = opt.vals.reduce((a,b) => (a+b), 0)
+      const row = [opt.name].concat(opt.vals).concat(score)
+      return <tr key={i}>{this.renderRow(row)}</tr>
+    })
+  }
+
+  renderRow(row) {
+    return row.map((cell, i) => (
+      <td key={i}>{cell}</td>
+    ))
+  }
+
   render() {
     return (
       <div className="container">
+        <header>
+          <h1>Matrix</h1>
+        </header>
+
+        <table>
+          <tbody>
+            {this.renderLabelRow()}
+            {this.renderRows()}
+          </tbody>
+        </table>
+
         <header>
           <h1>Todo List</h1>
         </header>
@@ -24,6 +55,17 @@ class App extends Component {
         </ul>
       </div>
     )
+  }
+
+  variables() {
+    return ['price', 'usability']
+  }
+
+  options() {
+    return [
+      {name: 'slack', vals: [5, 8]},
+      {name: 'IRC', vals: [10, 5]}
+    ]
   }
 }
 
