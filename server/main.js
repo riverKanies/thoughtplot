6417bf864ce79d976377f4fb05890f2d9a30db28
River Kanies <river.kanies@gmail.com>
2017-10-16 23:23:51 -0400

    remove decision deleting code

--- server/main.js
+++ server/main.js
@@ -2,11 +2,15 @@ import { Meteor } from 'meteor/meteor';
 import { Decisions } from '../imports/api/decisions.js'
 
 Meteor.startup(() => {
-  // code to run on server at startup
-  const decisions = Decisions.find().fetch()
-  //console.log('decisions', decisions)
-  decisions.forEach((dec)=>{
-    console.log('decz', dec)
-    Decisions.remove(dec._id)
-  })
+
 });
+
+
+// delete all decisions
+  // // code to run on server at startup
+  // const decisions = Decisions.find().fetch()
+  // //console.log('decisions', decisions)
+  // decisions.forEach((dec)=>{
+  //   console.log('decz', dec)
+  //   Decisions.remove(dec._id)
+  // })
