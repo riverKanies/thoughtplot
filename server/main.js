a0ba2fedfa50117fbb2943fb5fa955d891802669
River Kanies <river.kanies@gmail.com>
2017-10-16 23:18:31 -0400

    clear prod decisions for cell refactor (using startup function)

--- server/main.js
+++ server/main.js
@@ -1,6 +1,12 @@
 import { Meteor } from 'meteor/meteor';
-import '../imports/api/decisions.js'
+import { Decisions } from '../imports/api/decisions.js'
 
 Meteor.startup(() => {
   // code to run on server at startup
+  const decisions = Decisions.find().fetch()
+  //console.log('decisions', decisions)
+  decisions.forEach((dec)=>{
+    console.log('decz', dec)
+    Decisions.remove(dec._id)
+  })
 });
