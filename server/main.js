49af074a5652ca7d15bed1478920528a8c82095d
River Kanies <river.kanies@gmail.com>
2017-10-25 10:27:15 -0400

    reset db and add profile with collaborators on user creation

--- server/main.js
+++ server/main.js
@@ -1,7 +1,29 @@
 import { Meteor } from 'meteor/meteor';
 import { Decisions } from '../imports/api/decisions.js'
+import { Accounts } from 'meteor/accounts-base'
 
 Meteor.startup(() => {
+  const users = Meteor.users.find().fetch()
+  users.forEach(u=>{
+    if (!u.profile) {
+      Meteor.users.remove(u._id) //Meteor.users.findOne(u._id, {$set: {profile: {collaborators: []}}})
+    }
+  })
+  // code to run on server at startup
+  const decisions = Decisions.find().fetch()
+  //console.log('decisions', decisions)
+  decisions.forEach((dec)=>{
+    console.log('decz', dec)
+    Decisions.remove(dec._id)
+  })
+});
+
+Accounts.onCreateUser(function(options, user) {
+    if (user.profile == undefined) {
+      user.profile = {}
+      user.profile.collaborators = []
+    }
+    return user
 });
 
 
