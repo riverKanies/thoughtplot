9374fc789410b9aef1b7e6cb017a689c52787910
River Kanies <river.kanies@gmail.com>
2017-11-12 15:09:04 -0500

    WIP: cost or benefit on cellbuilder

--- imports/ui/matrix/CellBuilder.jsx
+++ imports/ui/matrix/CellBuilder.jsx
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from 'react'
 import colors from '../colors'
 
-const range = [-5,-4,-3,-2,-1,0,1,2,3,4,5]
+const range = [0,1,2,3,4,5]
+const fullRange = [-5,-4,-3,-2,-1,0,1,2,3,4,5]
 const btnStyles = {
   width: '25px',
   textAlign: 'center',
-  border: `1px solid ${colors.blue}`,
+  border: `1px solid lightgray`,
   borderRadius: '50%',
   background: 'white',
   margin: '3px'
@@ -30,18 +31,68 @@ export default class CellBuilder extends Component {
     this.state = {}
     this.state.currentRow = 1
     this.state.currentColumn = 1
+    this.state.isCostArray = []
 
     this.nextCell = this.nextCell.bind(this)
     this.selectBtn = this.selectBtn.bind(this)
+    this.toggleIsCost = this.toggleIsCost.bind(this)
   }
 
   renderScoreButtons(value) {
+    const isCost = this.isCost()
+    console.log('btns', isCost)
+    if (isCost) return <div className='row'>
+      {range.map((s) => {
+        const selectedStyles = (-s == value) ? {background: colors.orange} : {}
+        return <div key={s} className='col-1' style={{textAlign: 'center'}}><button style={{...btnStyles, ...selectedStyles, borderColor: colors.orange}} onClick={this.selectBtn(-s)}>{s}</button></div>
+      })}
+    </div>
+    if (!isCost && isCost !== false) return <div className='row'>
+      {fullRange.map((s) => {
+        const selectedStyles = (s == value) ? {background: (s<0 ? colors.orange : colors.blue)} : {}
+        return <div key={s} className='col-1' style={{textAlign: 'center'}}><button style={{...btnStyles, ...selectedStyles}} onClick={this.selectBtn(s)}>{s}</button></div>
+      })}
+    </div>
     return <div className='row'>
       {range.map((s) => {
         const selectedStyles = (s == value) ? {background: colors.blue} : {}
         return <div key={s} className='col-1' style={{textAlign: 'center'}}><button style={{...btnStyles, ...selectedStyles}} onClick={this.selectBtn(s)}>{s}</button></div>
       })}
     </div>
+  }
+
+  renderScoreDescription () {
+    const isCost = this.isCost()
+    const { mtx } = this.props
+    const i = this.state.currentRow
+    const j = this.state.currentColumn
+    const value = mtx[i][j].val
+    const option = mtx[i][0].val
+    const cell = mtx[0][j]
+    const consideration = cell.val
+    if (isCost) return <text>Based on <b style={{color: colors.blue}}>{consideration}</b> alone, how costly is <b style={{color: colors.orange}}>{option}</b>?
+    <br/><br/></text>
+    if (!isCost && isCost !== false) return <text>Based on <b style={{color: colors.blue}}>{consideration}</b> alone, the option <b style={{color: colors.orange}}>{option}</b> should get a score of:
+    <br/><br/></text>
+    return <text>Based on <b style={{color: colors.blue}}>{consideration}</b> alone, how beneficial is <b style={{color: colors.orange}}>{option}</b>?
+    <br/><br/></text>
+
+  }
+
+  renderScoreSection() {
+    const isCost = this.isCost()
+    const { mtx } = this.props
+    const i = this.state.currentRow
+    const j = this.state.currentColumn
+    const value = mtx[i][j].val
+    const option = mtx[i][0].val
+    const cell = mtx[0][j]
+    const consideration = cell.val
+    console.log('score', isCost)
+    return <text>
+      {this.renderScoreDescription()}
+      {this.renderScoreButtons(value)}
+    </text>
   }
 
   renderBuilder() {
@@ -51,29 +102,32 @@ export default class CellBuilder extends Component {
     const j = this.state.currentColumn
     const value = mtx[i][j].val
     const option = mtx[i][0].val
-    const consideration = mtx[0][j].val
+    const cell = mtx[0][j]
+    const consideration = cell.val
     const finished = (i==mtx.length-1 && j==mtx[0].length-1)
     const note = this.props.mtx[i][j].note
+    const toggleStyle = {background: colors.blue, color: 'white'}
     return <div>
       <p>
         <b style={{color: colors.orange}}>{option}</b>
         <b> - </b>
         <b style={{color: colors.blue}}>{consideration}</b>
       </p>
-      <text>Based on <b style={{color: colors.blue}}>{consideration}</b> alone, the option <b style={{color: colors.orange}}>{option}</b> should get a score of:
+      <p>Is <b style={{color: colors.blue}}>{consideration}</b> a cost or a benefit?
+        <button style={this.isCost() ? toggleStyle : {}} onClick={this.toggleIsCost(j,true)}>Cost</button>
+        <button style={this.isCost() === false ? toggleStyle : {}} onClick={this.toggleIsCost(j,false)}>Benefit</button>
+      </p>
+      {this.renderScoreSection()}
       <br/><br/>
-      {this.renderScoreButtons(value)}<br/>
-      <text style={asideStyles}>(note that a consideration like cost should get a negative score)</text>
-      </text><br/><br/>
       <label>Add a Note:</label><br/>
-      <textarea value={note} onChange={this.props.onChangeNote(i,j)} placeholder='this option will save time in the long run'/><br/>
-      <text style={asideStyles}>(describe why you think this score is appropriate)</text><br/>
+      <textarea value={note} onChange={this.props.onChangeNote(i,j)} placeholder='(describe why you think this score is appropriate)'/><br/>
       <br/><br/>
       {finished ? <text><button onClick={this.props.setTab('matrix')} style={nxtStyles}>View Plot &#8680;</button></text> : <button onClick={this.nextCell} style={nxtStyles}>Next Consideration &#8680;</button>}
     </div>
   }
 
   render() {
+    console.log('render',this.state.isCostArray)
     return (<div id='cellBuilder'>
       <p>
         <b>Score Options</b><br/>
@@ -88,9 +142,9 @@ export default class CellBuilder extends Component {
     const i = this.state.currentRow
     const j = this.state.currentColumn
     if (j==this.props.mtx[0].length-1) {
-      this.setState({currentRow: i+1, currentColumn: 1})
+      this.setState({currentRow: i+1, currentColumn: 1, isCost: this.props.mtx[0][1].isCost || null})
     } else {
-      this.setState({currentColumn: j+1})
+      this.setState({currentColumn: j+1, isCost: this.props.mtx[0][j+1].isCost || null})
     }
   }
 
@@ -100,6 +154,19 @@ export default class CellBuilder extends Component {
       const j = this.state.currentColumn
       this.props.onChangeHandler(i,j,s)
     }
+  }
+
+  toggleIsCost(j, isCost) {
+    return () => {
+      const { isCostArray } = this.state
+      isCostArray[j] = isCost
+      console.log('toggle', isCostArray)
+      this.setState({isCostArray: isCostArray})
+    }
+  }
+  
+  isCost() {
+    return this.state.isCostArray[this.state.currentColumn]
   }
 
 }
